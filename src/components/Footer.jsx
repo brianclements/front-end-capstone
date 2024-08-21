08f9b1421331a18ffe26ba4c116cfbeeea61d238
Brian Clements-Tiberio <brian@brianclements.net>
2024-08-21 12:37:45 -0700

    feat: convert footer to mobile first, cleanup formatting, finish content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,40 @@ const Footer = () => {
   return (
     <footer>
       <ul>
+        <div id="logo-div-desktop">
           <img src={lemonLogo} alt="Little Lemon logo"/>
+        </div>
         <div>
           <h3>Sitemap</h3>
           <Links/>
         </div>
         <div>
           <h3>Contact</h3>
+            <p>1234 Something Ave.</p>
+            <p>Chicago IL, 60007 </p>
         </div>
         <div>
           <h3>Social Media</h3>
+          <ul id="socials-list">
+            <li>
+              <a>
+                <icon class="fa fa-instagram"></icon>
+              </a>
+            </li>
+            <li>
+              <a>
+                <icon class="fa fa-twitter"></icon>
+              </a>
+            </li>
+            <li>
+              <a>
+                <icon class="fa fa-facebook"></icon>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="logo-div-mobile">
+          <img src={lemonLogo} alt="Little Lemon logo"/>
         </div>
       </ul>
     </footer>
