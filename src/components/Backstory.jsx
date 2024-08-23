062888a3ee0185ede3ae6db0e0fe77832bfc1f70
Brian Clements-Tiberio <brian@brianclements.net>
2024-08-23 15:02:46 -0700

    feat: <Backstory/> add pictures and improve fomatting

--- src/components/Backstory.jsx
+++ src/components/Backstory.jsx
@@ -1,11 +1,17 @@
 import style from './Backstory.module.css';
 import Bio from './Backstory.Bio'
+import shefImage from '../assets/images/restaurant_chef_B.jpg';
+import bbqImage from '../assets/images/bbq.jpg';
 
 const Backstory = () => {
   return (
-    <section id={style["backstory"]}>
+    <section>
       <div id={style["padding-div"]}>
         <Bio/>
+        <div id={style["image-div"]}>
+          <img src={shefImage} alt="Shef preparing a dish"/>
+          <img src={bbqImage} alt="Fish and vegitables on a BBQ"/>
+        </div>
       </div>
     </section>
   );
