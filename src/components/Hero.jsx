f88920859a3c8bd9cda6a41e1764b1d9a174c39e
Brian Clements-Tiberio <brian@brianclements.net>
2024-08-23 14:53:22 -0700

    feat: <Hero/> add picture and improve fomatting

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import style from './Hero.module.css';
 import Bio from './Hero.Bio'
+import foodTrayImage from '../assets/images/food_tray.jpg';
 
 const Hero = () => {
   return (
     <section id={style["hero-banner"]}>
       <div id={style["padding-div"]}>
         <Bio/>
+        <img src={foodTrayImage} alt="Shef holding a tray of food"/>
       </div>
     </section>
     );
