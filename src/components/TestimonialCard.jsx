06d750fd93c61ba94080529a00b6697b8aec1305
Brian Clements-Tiberio <brian@brianclements.net>
2024-08-25 11:43:55 -0700

    feat: create testimonials section with sample cards

---
+++ src/components/TestimonialCard.jsx
@@ -0,0 +1,38 @@
+import style from './TestimonialCard.module.css';
+import starImg from '../assets/images/star.jpeg';
+
+const TestimonialCard = (props) => {
+  const insertRating = (numStars) => {
+    const rating = [];
+      for (let i = numStars; i > 0; i--) {
+        rating.push(
+          <img src={starImg} alt="1 Star"/>
+        );
+      };
+
+      return (  
+        <>
+          {rating}
+        </>
+      );
+    };
+
+  return (
+    <article id={style.card}>
+      <div id={style["card-padding"]}>
+        <div id={style.rating}>
+          {insertRating(props.rating)}
+        </div>
+        <div id={style.reviewer}>
+          <img src={props.avatar} alt="Reviewer avatar"/>
+          <p className={style["multiline-ellipsis"]}>{props.userName}</p>
+        </div>
+        <div id={style.review} className={style["multiline-ellipsis"]}>
+          <p>{props.review}</p>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default TestimonialCard;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,43 @@
+import TestimonialCard from './TestimonialCard';
 import style from './Testimonials.module.css';
+import id1Avatar from '../assets/images/id1.jpeg';
+import id2Avatar from '../assets/images/id2.jpeg';
+import id3Avatar from '../assets/images/id3.jpeg';
+import id4Avatar from '../assets/images/id4.jpeg';
 
 const Testimonials = () => {
+  const userName = 'First Name Last Name'
+  const review = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed dui felis.'
   return (
     <section id={style["testimonials-section"]}>
       <div id={style["padding-div"]}>
         <h1 id={style["h1"]} className="markazi-text-display-title">Testimonials</h1>
+        <div id={style["testimonials-list"]}>
+          <TestimonialCard
+            avatar={id1Avatar}
+            userName={userName}
+            rating='1'
+            review={review}
+          />
+          <TestimonialCard
+            avatar={id2Avatar}
+            userName={userName}
+            rating='3'
+            review={review}
+          />
+          <TestimonialCard
+            avatar={id3Avatar}
+            userName={userName}
+            rating='4'
+            review={review}
+          />
+          <TestimonialCard
+            avatar={id4Avatar}
+            userName={userName}
+            rating='5'
+            review={review}
+          />
+        </div>
       </div>
     </section>
   );
