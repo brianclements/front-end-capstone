e77c8c928beda2c521db2c761ec50e2ec1cc5110
Brian Clements-Tiberio <brian@brianclements.net>
2024-08-27 10:11:52 -0700

    fix: improve some css formatting for testimonials cards

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -9,7 +9,7 @@ const Testimonials = () => {
   const userName = 'First Name Last Name'
   const review = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed dui felis.'
   return (
-    <section id={style["testimonials-section"]}>
+    <section id={style.section}>
       <div id={style["padding-div"]}>
         <h1 id={style["h1"]} className="markazi-text-display-title">Testimonials</h1>
         <div id={style["testimonials-list"]}>
