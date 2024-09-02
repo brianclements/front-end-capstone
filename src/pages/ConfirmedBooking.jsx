be29cd1357895903dc8ba42da092249ebc5a983b
Brian Clements-Tiberio <brian@brianclements.net>
2024-09-01 18:10:23 -0700

    feat: update/finish <ConfirmedBooking/> page

--- src/pages/ConfirmedBooking.jsx
+++ src/pages/ConfirmedBooking.jsx
@@ -1,12 +1,33 @@
 import Hero from '../components/Hero';
+import Reserve from '../components/Reserve';
+import Button from '../components/Button';
 
+import tableAreaImage from '../assets/images/restaurant.jpg';
 import './ConfirmedBooking.css';
+import reserveStyle from '../components/Reserve.module.css';
 
 const ConfirmedBooking = () => {
   return (
     <main>
-      <Hero/>
-      <h3>Booking Confirmed</h3>
+      <Hero
+        content={[
+          <Reserve style={reserveStyle}/>
+        ]}
+        image={
+          <img src={tableAreaImage} alt="Eating area outside"/>
+        }
+      />
+      <div id="booking-confirmed">
+        <h3>Booking Confirmed!</h3>
+        <Button 
+          text="Go Back"
+          navigateTo="/"
+          styles={[
+            "button-12x3_5rem",
+            "button-border-radius-8px"
+          ].join(" ")}
+        />
+        </div>
     </main>
   );
 };
